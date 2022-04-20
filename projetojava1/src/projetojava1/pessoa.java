2741d07aa71f626cd9ee92442349a958117d3c15
alsma <alsma@DESKTOP-NPPUQPB>
2022-04-20 17:58:50 -0300

    novo teste

--- projetojava1/src/projetojava1/pessoa.java
+++ projetojava1/src/projetojava1/pessoa.java
@@ -2,5 +2,25 @@ package projetojava1;
 
 public class pessoa {
 	Double x = 10.0;
+	
+	private String nome;
+
+	public Double getX() {
+		return x;
+	}
+
+	public void setX(Double x) {
+		this.x = x;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 
 }
