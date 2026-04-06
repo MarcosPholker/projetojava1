efb6bed300f516ba2135bcf10419332cf927bb64
marco <marco@DESKTOP-HQ6925N>
2026-04-06 20:33:20 -0300

    fazendo o primeiro commit para o repositorio local

---
+++ projetojava1/src/projeto/Projetojava1.java
@@ -0,0 +1,10 @@
+package projeto;
+
+public class Projetojava1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testando o sistema");
+	}
+
+}
