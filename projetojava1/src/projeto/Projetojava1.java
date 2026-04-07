5a7430262976fa5c7b725744a0b1472dc7025219
marco <marco@DESKTOP-HQ6925N>
2026-04-06 21:18:06 -0300

    teste3

--- projetojava1/src/projeto/Projetojava1.java
+++ projetojava1/src/projeto/Projetojava1.java
@@ -5,6 +5,7 @@ public class Projetojava1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("testando o sistema");
+		System.out.println("uma nova alteração no meu projeto");
 	}
 
 }
